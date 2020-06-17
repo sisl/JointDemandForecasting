c19c5a598bc6864517454cc5773de7b3089cc3c5
Arec <jamgochian95@gmail.com>
2020-06-16 20:08:31 -0700

    adding options to learn full covariance matrix

--- CalibratedTimeseriesModels/models/gnn.py
+++ CalibratedTimeseriesModels/models/gnn.py
@@ -2,14 +2,15 @@ import torch
 import torch.nn as nn
 from CalibratedTimeseriesModels.abstractmodels import *
 from torch.distributions.normal import Normal as normal
+from torch.distributions.multivariate_normal import MultivariateNormal as mvnormal
 
 class GaussianNeuralNet(ExplicitPredictiveModel):
     """ 
 
-    Class for probabilistic feedforward neural network using single isotropic MvGaussian. 
+    Class for probabilistic feedforward neural network using single MvGaussian. 
     
     """     
-    def __init__(self, input_dim, input_horizon, hidden_layer_dims, output_dim, prediction_horizon):
+    def __init__(self, input_dim, input_horizon, hidden_layer_dims, output_dim, prediction_horizon, full_covariance=False):
         """ 
 
         Initializes autoregressive, probabilistic feedforward neural network model. 
@@ -21,7 +22,7 @@ class GaussianNeuralNet(ExplicitPredictiveModel):
             hidden_layer_dims (list of ints): the hidden layer sizes in the neural network
             output_dim (int): the output dimension
             prediction_horizon (int): the prediction horizon K
-
+            full_covariance (bool): if true predict a full covariance matrix, otherwise a diagonal one
         """ 
         super(GaussianNeuralNet, self).__init__()
         self.input_dim = input_dim
@@ -29,6 +30,7 @@ class GaussianNeuralNet(ExplicitPredictiveModel):
         self.hidden_layer_dims = hidden_layer_sizes
         self.output_dim = output_dim
         self.K = prediction_horizon
+        self.full_covariance = full_covariance
         
         fc_net = []
         fc_sizes = np.append(self.input_dim * self.T, self.hidden_layer_dims)
@@ -36,7 +38,15 @@ class GaussianNeuralNet(ExplicitPredictiveModel):
             fc_net.append(nn.Linear(in_features=fc_sizes[i], out_features=fc_sizes[i+1]))
             fc_net.append(nn.LeakyReLU())
         
-        fc_net.append(nn.Linear(in_features=fc_sizes[-1], out_features=2*self.output_size*self.K))
+        
+        self._num_means = self.output_size*self.K
+        if self.full_covariance:
+            n = self.output_size*self.K
+            self._num_cov = n*(n+1)/2
+        else:
+            self._num_cov = self.output_size*self.K
+        
+        fc_net.append(nn.Linear(in_features=fc_sizes[-1], out_features=self._num_means+self._num_cov))
         self.fc = nn.Sequential(*fc_net)
         
     def forward(self, y, u, K):
@@ -55,11 +65,24 @@ class GaussianNeuralNet(ExplicitPredictiveModel):
         inputs = y.reshape((B, T*ydim))
         outputs = self.fc(inputs)
         B, outdims = outputs.shape
-        mu = outputs[:,:outdims//2]
-        sig = nn.functional.softplus(outputs[:,outdims//2:])
+        mu = outputs[:,:self._num_means]
+        
+        # full covariance matrix
+        if self.full_covariance:
+            diag = nn.functional.softplus(outputs[:,self._num_means:2*self._num_means])
+            offdiag = outputs[:,2*self._num_means:]
+            
+            L = torch.zeros(B,self._num_means,self._num_means)
+            indices = torch.tril_indices(self._num_means, self._num_means, -1)
+            L[:,torch.arange(self._num_means),torch.arange(self._num_means)] = diag
+            L[:,indices[0], indices[1]] = offdiag
+            
+            dist = mvnormal(loc=mu, scale_tril=L)
         
         # isotropic normal distribution
-        dist = normal(loc=mu, scale=sig)
+        else:
+            sig = nn.functional.softplus(outputs[:,self._num_means:])
+            dist = normal(loc=mu, scale=sig)
         return dist
     
 class GaussianLSTM(ExplicitPredictiveModel):
@@ -69,7 +92,7 @@ class GaussianLSTM(ExplicitPredictiveModel):
     
     """ 
     def __init__(self, input_dim, hidden_dim, fc_hidden_layer_dims, output_dim, prediction_horizon,
-                 num_layers=1, dropout=0.0, bidirectional=False, random_start=True):
+                 full_covariance=False, num_layers=1, dropout=0.0, bidirectional=False, random_start=True):
         """ 
 
         Initializes sequence-to-sequence LSTM model. 
@@ -81,6 +104,7 @@ class GaussianLSTM(ExplicitPredictiveModel):
             fc_hidden_layer_dims (list of ints): the hidden layer sizes in the neural network mapping from hidden state to output
             output_dim (int): the dimension of the outputs at each point in the sequence
             prediction_horizon (int): the prediction horizon K
+            full_covariance (bool): if true predict a full covariance matrix, otherwise a diagonal one
             num_layers (int): number of layers in a possibly stacked LSTM
             dropout (float): the dropout rate of the lstm
             bidirectional (bool): whether to initialize a bidirectional lstm
@@ -94,6 +118,7 @@ class GaussianLSTM(ExplicitPredictiveModel):
         self.fc_hidden_layer_dims = fc_hidden_layer_dims
         self.output_dim = output_dim
         self.K = prediction_horizon
+        self.full_covariance = full_covariance
         
         # LSTM parameters
         self.num_layers = num_layers
@@ -111,8 +136,14 @@ class GaussianLSTM(ExplicitPredictiveModel):
             fc_net.append(nn.Linear(in_features=fc_sizes[i], out_features=fc_sizes[i+1]))
             fc_net.append(nn.LeakyReLU())
         
-        # output twice the features for mu, sigma for each time in horizon K
-        fc_net.append(nn.Linear(in_features=fc_sizes[-1], out_features = 2 * self.output_dim * self.K))
+        self._num_means = self.output_size*self.K
+        if self.full_covariance:
+            n = self.output_size*self.K
+            self._num_cov = n*(n+1)/2
+        else:
+            self._num_cov = self.output_size*self.K
+            
+        fc_net.append(nn.Linear(in_features=fc_sizes[-1], out_features=self._num_mean+self._num_cov))
         self.fc = nn.Sequential(*fc_net)
         
     def forward(self, y, u, K):
@@ -180,9 +211,23 @@ class GaussianLSTM(ExplicitPredictiveModel):
         outputs = self.fc(h_n)
         # outputs will be (B, 2*K*ydims)
         B, outdims = fc_output.shape
-        mu = outputs[:,:outdims//2]
-        sig = nn.functional.softplus(outputs[:,outdims//2:])
+        mu = outputs[:,:self._num_means]
+        
+        # full covariance matrix
+        if self.full_covariance:
+            diag = nn.functional.softplus(outputs[:,self._num_means:2*self._num_means])
+            offdiag = outputs[:,2*self._num_means:]
+            
+            L = torch.zeros(B,self._num_means,self._num_means)
+            indices = torch.tril_indices(self._num_means, self._num_means, -1)
+            L[:,torch.arange(self._num_means),torch.arange(self._num_means)] = diag
+            L[:,indices[0], indices[1]] = offdiag
+            
+            dist = mvnormal(loc=mu, scale_tril=L)
         
         # isotropic normal distribution
-        dist = normal(loc=mu, scale=sig)
+        else:
+            sig = nn.functional.softplus(outputs[:,self._num_means:])
+            dist = normal(loc=mu, scale=sig)
         return dist
+        
